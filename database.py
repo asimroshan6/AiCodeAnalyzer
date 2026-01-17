4337c682b6ad9939dd4b1aba8a2add7ebbcf157c
Asim Roshan <asimroshan6@gmail.com>
2026-01-17 14:46:59 +0530

    fixed database

--- database.py
+++ database.py
@@ -4,6 +4,7 @@ from config import settings
 
 engine = create_engine(
     url=settings.DATABASE_URL,
+    connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
